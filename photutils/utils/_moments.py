3f96871e0a8fb5044103390495dc2fc10fa69242
Larry Bradley <larry.bradley@gmail.com>
2025-05-16 20:20:02 -0400

    WIP: Add weights and mask options for image moments

--- photutils/utils/_moments.py
+++ photutils/utils/_moments.py
@@ -5,12 +5,15 @@ This module provide tools for calculating image moments.
 
 import numpy as np
 
-__all__ = ['_moments', '_moments_central']
+__all__ = ['image_moments']
 
 
-def _moments_central(data, center=None, order=1):
+def image_moments(data, center=(0, 0), order=1, weights=None, mask=None):
     """
-    Calculate the central image moments up to the specified order.
+    Calculate the weighted image moments up to the specified order.
+
+    Raw moments correspond to center=(0, 0). A different center can be
+    input to compute central moments.
 
     Parameters
     ----------
@@ -18,50 +21,50 @@ def _moments_central(data, center=None, order=1):
         The input 2D array.
 
     center : tuple of two floats or `None`, optional
-        The ``(x, y)`` center position. If `None` it will calculated as
-        the "center of mass" of the input ``data``.
+        The ``(x, y)`` center position.
 
     order : int, optional
         The maximum order of the moments to calculate.
 
+    weights : 2D array_like or None, optional
+        An array of weights with the same shape as data.
+
+    mask : 2D boolean array or None, optional
+        A boolean array with the same shape as ``data``, where a `True`
+        value indicates the corresponding element of ``data`` is masked.
+        Masked pixels are ignored when computing the moments.
+
     Returns
     -------
     moments : 2D `~numpy.ndarray`
-        The central image moments.
+        The image moments.
     """
-    data = np.asarray(data).astype(float)
+    data = np.asarray(data, dtype=float)
 
     if data.ndim != 2:
         msg = 'data must be a 2D array'
         raise ValueError(msg)
 
-    if center is None:
-        from photutils.centroids import centroid_com
+    if weights is not None:
+        weights = np.asarray(weights, dtype=float)
+        if weights.shape != data.shape:
+            raise ValueError('weights must have the same shape as data.')
 
-        center = centroid_com(data)
+    bad_mask = ~np.isfinite(data)
+    if weights is not None:
+        bad_mask |= ~np.isfinite(weights)
+    if mask is not None:
+        if mask.shape != data.shape:
+            raise ValueError('mask must have the same shape as data.')
+        bad_mask |= mask
+
+    data = np.where(bad_mask, 0.0, data)
+    if weights is not None:
+        weights = np.where(bad_mask, 0.0, weights)
+        data *= weights
 
     indices = np.ogrid[tuple(slice(0, i) for i in data.shape)]
     ypowers = (indices[0] - center[1]) ** np.arange(order + 1)
     xpowers = np.transpose(indices[1] - center[0]) ** np.arange(order + 1)
 
     return np.dot(np.dot(np.transpose(ypowers), data), xpowers)
-
-
-def _moments(data, order=1):
-    """
-    Calculate the raw image moments up to the specified order.
-
-    Parameters
-    ----------
-    data : 2D array_like
-        The input 2D array.
-
-    order : int, optional
-        The maximum order of the moments to calculate.
-
-    Returns
-    -------
-    moments : 2D `~numpy.ndarray`
-        The raw image moments.
-    """
-    return _moments_central(data, center=(0, 0), order=order)
