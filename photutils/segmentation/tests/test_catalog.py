e1e615ef7daabb7326af2eca3de81b24db8a981b
Larry Bradley <larry.bradley@gmail.com>
2026-04-30 22:01:15 -0400

    Ensure eccentricity, ellipticity, and elongation do not have units

--- photutils/segmentation/tests/test_catalog.py
+++ photutils/segmentation/tests/test_catalog.py
@@ -3676,6 +3676,17 @@ def test_sky_orientation_column():
     assert_allclose(tbl['sky_orientation'], cat.sky_orientation)
 
 
+def test_ellipse_units(gauss_101_catalog):
+    """
+    Test that the eccentricity, ellipticity, and elongation values are
+    not Quantity objects (with dimensionless_unscaled unit).
+    """
+    _data, _segm, cat = gauss_101_catalog
+    assert not isinstance(cat.eccentricity, u.Quantity)
+    assert not isinstance(cat.ellipticity, u.Quantity)
+    assert not isinstance(cat.elongation, u.Quantity)
+
+
 class TestPartialPixelErrorWeights:
     """
     Tests that aperture flux errors weight the pixel variances by the
